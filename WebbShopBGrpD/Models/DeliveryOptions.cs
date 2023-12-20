4938ef818e29f53aaa92591dc2ae2663f4d385ad
miramaarit <mirker76@hotmail.com>
2023-12-20 14:23:37 +0100

    Create DeliveryOptions.cs
    DeliveryOptions class

---
+++ WebbShopBGrpD/Models/DeliveryOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebbShopBGrpD.Models
+{
+    internal class DeliveryOptions
+    {
+        public int Id{ get; set; }
+
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+    }
+}
