36a309c0d36e624de932f281e503095aadd14fc0
miramaarit <mirker76@hotmail.com>
2023-12-20 14:05:35 +0100

    Create ProductSupplier.cs
    ProductSupplier class

---
+++ WebbShopBGrpD/Models/ProductSupplier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebbShopBGrpD.Models
+{
+    internal class ProductSupplier
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string StreetAdress { get; set; }
+
+        public string City { get; set; }
+
+        public string Country{ get; set; }
+    }
+}
