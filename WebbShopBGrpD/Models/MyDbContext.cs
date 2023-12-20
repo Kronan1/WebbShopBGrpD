3ae261c6098382e9fb9b4cd13071d6240afc0919
Kronan1 <simonlindberg1992@gmail.com>
2023-12-20 14:37:50 +0100

    Add MyDbContext

---
+++ WebbShopBGrpD/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebbShopBGrpD.Models
+{
+    internal class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=WebbShopB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
