85bf69b24aaa2a2f72f0012d9fbc302240770077
Kronan1 <simonlindberg1992@gmail.com>
2024-01-08 11:28:27 +0100

    Added Enums and dict

---
+++ WebbShopBGrpD/MyEnums.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebbShopBGrpD
+{
+    internal class MyEnums
+    {
+        public enum DeliveryOption
+        {
+            Postnord = 50,
+            Dhl = 75,
+            Schenker = 60,
+            Bring = 49,
+            EarlyBird = 64
+        }
+
+
+        public enum PaymentOption
+        {
+            Klarna,
+            Swish,
+            Kreditkort,
+            Faktura
+        }
+
+        public enum Gender
+        {
+            Man,
+            Kvinna,
+            Unisex
+        }
+
+
+    }
+}
--- WebbShopBGrpD/Program.cs
+++ WebbShopBGrpD/Program.cs
@@ -26,51 +26,27 @@ namespace WebbShopBGrpD
             //    }
             //);
 
-            //myDb.AddRange(
-            //    new ProductCategory
-            //    {
-            //        Name = "T-Shirt"
-            //    }
-            //);
-
-            //var gender = myDb.Genders.First();
-            //var category = myDb.ProductCategories.First();
-            //var supplier = myDb.ProductSuppliers.First();
-
-            //myDb.AddRange(
-            //new Models.Product
-            //{
-            //    Name = "Gulspetta",
-            //    Price = 100,
-            //    Info = "Grön i bomull",
-            //    Quantity = 25,
-            //    Gender = gender,
-            //    ProductCategory = category,
-            //    Supplier = supplier,
-            //});
-            //    var gender = myDb.Genders.FirstOrDefault(g => g.Id == 1);
-            //    var category = myDb.ProductCategories.FirstOrDefault(c => c.Id == 1);
-            //    var supplier = myDb.ProductSuppliers.FirstOrDefault(s => s.Id == 1);
-
-            //    myDb.AddRange(
-            //    new Models.Product
-            //    {
-            //        Name = "Glugg",
-            //        Price = 75,
-            //        Info = "Röd i bomull",
-            //        Quantity = 30,
-            //        Gender = gender,
-            //        ProductCategory = category,
-            //        Supplier = supplier
-            //    });
-
-            //    myDb.SaveChanges();
-            //};
 
             #endregion
 
-            
-            
+            Dictionary<string, float> PaymentOptionPrice = new Dictionary<string, float>();
+
+            PaymentOptionPrice.Add(MyEnums.DeliveryOption.Postnord.ToString(), 50);
+            PaymentOptionPrice.Add(MyEnums.DeliveryOption.Dhl.ToString(), 75);
+            PaymentOptionPrice.Add(MyEnums.DeliveryOption.Schenker.ToString(), 60);
+            PaymentOptionPrice.Add(MyEnums.DeliveryOption.Bring.ToString(), 49);
+            PaymentOptionPrice.Add(MyEnums.DeliveryOption.EarlyBird.ToString(), 64);
+
+            while (true)
+            {
+                Console.WriteLine((MyEnums.DeliveryOption.Postnord.ToString()));
+                Console.WriteLine(MyEnums.Gender.Man);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+
+
         }
     }
 }
