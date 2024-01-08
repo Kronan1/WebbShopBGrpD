68ed5f55e5386303fcb12dfcef7de13a8c833c6e
Kronan1 <simonlindberg1992@gmail.com>
2024-01-08 11:50:53 +0100

    MenuBar

---
+++ WebbShopBGrpD/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowDemo;
+
+namespace WebbShopBGrpD
+{
+    internal class Menu
+    {
+        public static void MenuBar()
+        {
+            List<string> topText = new List<string> { "# Fina butiken #", "Allt inom kläder" };
+            var windowTop = new Window("", 2, 1, topText);
+            windowTop.Left = 45;
+            windowTop.Draw();
+
+            //List<string> navigationText = new List<string> { "[1] = Start", "[2] = Shop", "[3] = Varukorg", "[4] = Admin" };
+            List<string> navigationText = new List<string> { "[1] = Start [2] = Shop [3] = Varukorg [4] = Admin" };
+            var windowNavigation = new Window("", 2, 5, navigationText);
+            windowNavigation.Left = 28;
+            windowNavigation.Draw();
+
+
+        }
+        
+    }
+}
--- WebbShopBGrpD/Program.cs
+++ WebbShopBGrpD/Program.cs
@@ -1,4 +1,5 @@
 ﻿using WebbShopBGrpD.Models;
+using WindowDemo;
 
 namespace WebbShopBGrpD
 {
@@ -29,6 +30,10 @@ namespace WebbShopBGrpD
 
             #endregion
 
+            Menu.MenuBar();
+
+            Console.ReadLine();
+
             Dictionary<string, float> PaymentOptionPrice = new Dictionary<string, float>();
 
             PaymentOptionPrice.Add(MyEnums.DeliveryOption.Postnord.ToString(), 50);
@@ -45,6 +50,33 @@ namespace WebbShopBGrpD
                 Console.WriteLine();
                 Console.ReadLine();
             }
+
+
+            // Hämtar från databasen
+            List<string> categoriesText = new List<string> { "Byxor", "Tröjor", "Skor", "Skjortor", "xxxxxxxxxxxxxxxxxxxxxxx" };
+            //foreach(var text in categoriesText)
+            //{
+            //    Console.WriteLine(text);
+            //}
+
+            // Detta hämtas från databas
+            List<string> cartText = new List<string> { "1 st Blå byxor", "1 st Grön tröja", "1 st Röd skjorta" };
+            var windowCart = new Window("Din varukorg", 30, 6, cartText);
+            windowCart.Draw();
+
+            var windowCategories = new Window("Kategorier", 2, 6, categoriesText);
+            windowCategories.Draw();
+
+            List<string> topText = new List<string> { "# Fina butiken #", "Allt inom kläder" };
+            var windowTop = new Window("", 2, 1, topText);
+            windowTop.Draw();
+
+            windowTop.Left = 45;
+            windowTop.Draw();
+
+            Console.WriteLine("Nån annan text");
+
+            Console.ReadLine();
 
 
         }
