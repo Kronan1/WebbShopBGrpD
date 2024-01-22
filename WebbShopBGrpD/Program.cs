83cd5cd86af24305c75c29968e78d800170f9e92
Kronan1 <simonlindberg1992@gmail.com>
2024-01-22 14:45:19 +0100

    Added queries

--- WebbShopBGrpD/Program.cs
+++ WebbShopBGrpD/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Azure;
+using Dapper;
 using Microsoft.Data.SqlClient;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using WebbShopBGrpD.Models;
+using WebbShopBGrpD.Queries;
 using WindowDemo;
 
 namespace WebbShopBGrpD
@@ -16,19 +18,40 @@ namespace WebbShopBGrpD
             Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("sv-SE");
 
             // Kör en gång för att skapa data
-            //Data.CreateData(); 
+            // Data.CreateData();
+            // Console.ReadLine();
 
 
-            //string connectionString = "Server=.\\SQLExpress;Database=WebbShopB;Trusted_Connection=True;TrustServerCertificate=True;";
+            string connectionstring = "server=.\\sqlexpress;database=webbshopb;trusted_connection=true;trustservercertificate=true;";
 
-            //using (var connection = new SqlConnection(connectionString))
-            //{
-            //    connection.Open();
 
-            //    var sql = "SELECT AVG()";
+            // Most expensive product
+            using (var connection = new SqlConnection(connectionstring))
+            {
+                connection.Open();
 
-                
-            //}
+                // Dyraste produkten/produkterna
+                var sql1 = "SELECT * FROM Products WHERE Price = (SELECT TOP 1 Price FROM Products ORDER BY Price DESC)";
+                var result1 = connection.QuerySingleOrDefault<Product>(sql1);
+
+                // Hur många som sålts av varje produkt
+                var sql2 = "SELECT ProductId, " +
+                    "SUM (Quantity) AS 'TotalQuantitySold'" +
+                    " FROM PurchasedArticles " +
+                    "group by ProductId " +
+                    "ORDER by TotalQuantitySold DESC";
+                var result2 = connection.Query<ProductsSold>(sql2);
+
+                // Genomsnittlig ålder på kunder
+                var sql3 = "SELECT AVG(Age * 1.0) FROM Customers";
+                var result3 = connection.QueryFirstOrDefault<double>(sql3);
+
+                // Tar fram alla produkter som innehåller bomull
+                var sql4 = "SELECT * FROM Products WHERE Info LIKE '%Bomull%' OR Info LIKE '%bomull%'";
+                var result4 = connection.Query<Product>(sql4);
+
+                int test = 0;
+            }
 
 
             Menu menu = new Menu();
---
+++ WebbShopBGrpD/Queries/ProductsSold.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebbShopBGrpD.Queries
+{
+    internal class ProductsSold
+    {
+        public int ProductId { get; set; }
+        public int TotalQuantitySold { get; set; }
+    }
+}
